023bc71441725aad43b9822d3698567656e6d5ee
hexdump <hexdump0815@googlemail.com>
2025-10-28 13:08:50 +0100

    7c_woa: workaround for the keyboard not working
    it looks like the evdev input driver has to be used to properly detect
    the keyboard on the samsung galaxy book go now ...
    see: https://github.com/velvet-os/imagebuilder/issues/136#issuecomment-3448921421
    and: https://github.com/velvet-os/imagebuilder/discussions/320#discussioncomment-14762677

--- systems/snapdragon_7c_woa/postinstall-chroot.sh
+++ systems/snapdragon_7c_woa/postinstall-chroot.sh
@@ -2,3 +2,8 @@
 
 # install the qcom tools qrtr-ns and rmtfs
 apt-get -y install protection-domain-mapper qrtr-tools rmtfs tqftpserv
+
+# workaround for the keyboard not working
+# see: https://github.com/velvet-os/imagebuilder/issues/136#issuecomment-3448921421
+# and: https://github.com/velvet-os/imagebuilder/discussions/320#discussioncomment-14762677
+apt-get -y install xserver-xorg-input-evdev
--- systems/snapdragon_7c_woa/postinstall.sh
+++ systems/snapdragon_7c_woa/postinstall.sh
@@ -4,3 +4,7 @@ cp -v etc/X11/xorg.conf.d.samples/11-fbdev.conf etc/X11/xorg.conf.d
 # later after installing the firmware files from windows, this is used instead
 #cp -v etc/X11/xorg.conf.d.samples/11-modesetting.conf etc/X11/xorg.conf.d
 cp -v etc/X11/xorg.conf.d.samples/51-touchpad.conf etc/X11/xorg.conf.d
+# workaround for the keyboard not working
+# see: https://github.com/velvet-os/imagebuilder/issues/136#issuecomment-3448921421
+# and: https://github.com/velvet-os/imagebuilder/discussions/320#discussioncomment-14762677
+cp -v etc/X11/xorg.conf.d.samples/51-kbd-via-evdev-input.conf etc/X11/xorg.conf.d
