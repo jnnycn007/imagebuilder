f6f7fa3450a1e0df4d1aa93821bfc469e0e004ad
hexdump <hexdump0815@googlemail.com>
2026-03-08 00:17:56 +0100

    files: remove jack install and remove scripts
    now that we default to pipewire, jack can be handled by that as well via
    pw-jack after installing pipewire-jack

--- files/extra-files/scripts/install-jack.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get -y install a2jmidid qjackctl pulseaudio-module-jack
--- files/extra-files/scripts/remove-jack.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-apt-get -y remove --purge a2jmidid qjackctl pulseaudio-module-jack
-
-apt-get -y auto-remove
