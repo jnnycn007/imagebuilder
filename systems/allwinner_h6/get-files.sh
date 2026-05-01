80c5d362271beb1b2f1db2e67b7addb57a896301
hexdump <hexdump0815@googlemail.com>
2026-05-02 00:04:27 +0200

    h6: remove old noname box special u-boot
    this box is now booting fine with the v2026.04 u-boot, so no more
    special u-boot required for some boxes ... another box which also
    required a special u-boot now also boots with the new version just fine

--- systems/allwinner_h6/get-files.sh
+++ systems/allwinner_h6/get-files.sh
@@ -3,7 +3,6 @@
 
 allwinner_h6_release_version="6.12.12-stb-ah6%2B"
 allwinner_h6_uboot_version="211229-01"
-allwinner_h6_uboot_noname_version="200718-01"
 
 rm -f ${DOWNLOAD_DIR}/kernel-allwinner_h6-${2}.tar.gz
 wget -v https://github.com/hexdump0815/linux-mainline-and-mali-allwinner-h6-kernel/releases/download/${allwinner_h6_release_version}/${allwinner_h6_release_version}.tar.gz -O ${DOWNLOAD_DIR}/kernel-allwinner_h6-${2}.tar.gz
@@ -14,8 +13,6 @@ mkdir -p ${DOWNLOAD_DIR}/boot-extra-${1}
 for system in eachlink_h6_mini orange_pi_3 pine-h64 orangepi_one_plus tanix_tx6; do
   wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h6_uboot_version}/boot-${system}-aarch64.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-${system}.dd
 done
-# it looks like the newer noname u-boot does not work as expected, so use the older one until fresh ones get rebuilt one day ...
-wget -v https://github.com/hexdump0815/u-boot-misc/releases/download/${allwinner_h6_uboot_noname_version}/boot-allwinner_h6-aarch64-noname-tv-box.dd.gz -O - | gunzip -c > ${DOWNLOAD_DIR}/boot-extra-${1}/boot-noname_h6_tv_box.dd
 
 # use the eachlink h6 mini u-boot as default
 cp ${DOWNLOAD_DIR}/boot-extra-${1}/boot-eachlink_h6_mini.dd ${DOWNLOAD_DIR}/boot-allwinner_h6-${2}.dd
